41e26e0aeeae945007d91264a29a9f28bc78ebbb
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-03-05 21:19:25 +0530

    program for assignment2.4

---
+++ Assignment2.4.py
@@ -0,0 +1,6 @@
+
+print('WE, THE PEOPLE OF INDIA,\r')
+print("\thaving solemnly resolved to constitute India into a SOVEREIGN,\r")
+print("\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\r")
+print("\t\tand to secure to all its citizens\r")
+
